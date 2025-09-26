cb6761319976aa69466cf6316c83e0832a87c7d7
Luiz <luiz.barbosa.silva20@escola.pr.gov.br>
2025-09-26 08:38:15 -0300

    Inicializando servidor

--- main.py
+++ main.py
@@ -1,11 +1,49 @@
 from mysql.connector import (connection)
 from mysql.connector import errorcode
 import os
-from load dontenv import load_dontenv
+from dotenv import load_dotenv
 
-bd_conexao = connection.MySQLConnection(
-    host='localhost',
-    user='root',
-    password=senha,
-    database='bd_python'
-)
+load_dotenv()
+senha = os.getenv('SENHA_SQL')
+
+try:
+    bd_conexao = connection.MySQLConnection(
+        host='localhost',
+        user='root',
+        password=senha,
+        database='bd_python'
+    )
+    print("Conexão bem sucedida!")
+except mysql.connector.Error as erro:
+    if erro.errno == errorcode.ER_BAD_DB_ERROR:
+        print("O banco de dados não existe!")
+    elif erro.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+        print("Usuário ou senha incorretos!")
+    else:
+        print(erro)
+
+comando = bd_conexao.cursor()
+select = comando.execute("SELECT * FROM ALUNOS")
+resultado = comando.fetchall()
+for linha in resultado: 
+    print(linha)
+
+## INSERIR ALUNO
+sql_insert = "INSERT INTO ALUNOS (nome, ano) VALUES (%s, %s)"
+valores1 = ('Leonardo', '3 TEC')
+valores2 = ('João', '2 TEC')
+
+comando.execute(sql_insert, valores1)
+comando.execute(sql_insert, valores2)
+
+print("\n===TESTE INSERT===\n")
+comando = bd_conexao.cursor()
+select = comando.execute("SELECT * FROM ALUNOS")
+resultado = comando.fetchall()
+for linha in resultado: 
+    print(linha)
+
+##SEMPRE FECHAR A CONEXÃO
+comando.close()
+bd_conexao.commit()
+bd_conexao.close()
